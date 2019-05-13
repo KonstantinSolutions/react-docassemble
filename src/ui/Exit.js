5b00da1861e678f8244fa267675bb686c44d93b0
kiran <kiran@neostack.com>
2019-05-13 18:25:36 +0530

    added bootstrap

--- src/ui/Exit.js
+++ src/ui/Exit.js
@@ -2,7 +2,11 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 function Exit({ exit_label, exit_link, history }) {
-  return <button onClick={() => history.push(exit_link)}>{exit_label}</button>;
+  return (
+    <button className="btn btn-danger" onClick={() => history.push(exit_link)}>
+      {exit_label}
+    </button>
+  );
 }
 
 export default withRouter(Exit);
