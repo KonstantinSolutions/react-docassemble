03d97f52521439dbc264316595900f82281475f3
kiran <kiran@neostack.com>
2019-05-14 17:33:38 +0530

    store session data in local storage

--- src/context/index.js
+++ src/context/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import useLocalStorage from "react-use-localstorage";
 
 export const InterviewContext = React.createContext({});
 
 export function InterviewProvider(props) {
-  const [session, setSession] = useState();
-  const [i, setFilename] = useState();
+  const [session, setSession] = useLocalStorage("session", null);
+  const [i, setFilename] = useLocalStorage("filename", null);
   return (
     <InterviewContext.Provider value={{ session, setSession, i, setFilename }}>
       {props.children}
