bd96170e54f5b307ad002c865201630cc2d32ea0
kiran <kiran@neostack.com>
2019-05-21 19:16:36 +0530

    update start interview to customize button

--- src/ui/StartInterview.js
+++ src/ui/StartInterview.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { InterviewContext } from '../context';
 
-export function StartInterview({ filename, title, onStart }) {
+export function StartInterview({ filename, onStart, children }) {
   const { setSession, setFilename } = useContext(InterviewContext);
   function startInterview() {
     fetch(`/docassemble/api/session/new?i=${filename}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data, 'Data!');
+        console.log(data, 'Datar33!!');
         setSession(data.session);
         setFilename(filename);
         onStart();
       });
   }
-  return <button onClick={startInterview}>{title}</button>;
+  return React.cloneElement(children, { onClick: startInterview })
 }
