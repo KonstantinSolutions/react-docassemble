08ba6e53e3bbc132dbbdf857c20d4dff458ae868
kiran <kiran@neostack.com>
2019-05-26 17:37:18 +0530

    cleanup

--- src/api.js
+++ src/api.js
@@ -11,30 +11,3 @@ export function post(url, data) {
     body: JSON.stringify(data)
   }).then(res => res.json());
 }
-
-function fetchQuestion({i, session, setQuestion}) {
-  fetch(`/docassemble/api/session/question?i=${i}&session=${session}`)
-    .then(res => res.json())
-    .then(data => {
-      setQuestion(data);
-    });
-}
-
-function goBack({i, session, setQuestion}) {
-  post(`/docassemble/api/session/back`, data).then(data => {
-    setQuestion(data);
-  });
-}
-
-function saveVariables({isValid, i, session, variables, setQuestion}) {
-  if (!isValid()) {
-    return;
-  }
-  post(`/docassemble/api/session`, {
-    i,
-    session,
-    variables
-  }).then(data => {
-    setQuestion(data);
-  });
-}
