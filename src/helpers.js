790d89674e5c557f0a3841d766cf67d20a444a54
kiran <kiran@neostack.com>
2019-05-30 19:23:32 +0530

    added email validation

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,17 @@
+import {isEmail} from 'validator';
+
 const defaultValidators = {
-  required: value => (!value ? 'You need to fill this in.' : null)
+  required: value => (!value ? 'You need to fill this in.' : null),
+  email: value => (!isEmail(value || '') ? 'Not a valid email' : null)
 };
 
 function getValidators(field) {
   let validators = [];
   if (field.required) {
     validators = validators.concat(defaultValidators.required);
+  }
+  if (field.datatype === 'email') {
+    validators = validators.concat(defaultValidators.email);
   }
   return validators;
 }
@@ -17,6 +23,7 @@ export function validate(question, variables) {
     const fieldErrors = validators
       .map(validator => validator(value))
       .filter(error => Boolean(error));
+    console.log(field, fieldErrors);
     if (fieldErrors.length > 0) {
       acc[field.variable_name] = fieldErrors[0];
     }
