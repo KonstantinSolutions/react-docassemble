9dbe27ba8a5ba39cb7e7a5c8a035d6ac59ee3dd2
kiran <kiran@neostack.com>
2019-08-02 17:20:12 +0530

    show if check for required

--- src/helpers.js
+++ src/helpers.js
@@ -21,9 +21,26 @@ function getValidators(field) {
   return validators;
 }
 
+export function showIfCheck({ field, variables }) {
+  const { show_if_var, show_if_val } = field;
+  if (show_if_var) {
+    const booleanCheck =
+      show_if_val === "True" && Boolean(variables[show_if_var]);
+    const valueCheck =
+      show_if_val !== "True" && variables[show_if_var] === show_if_val;
+    if (!(booleanCheck || valueCheck)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 export function validate(question, variables) {
   const errors = question.fields.reduce((acc, field) => {
     const value = variables[field.variable_name];
+    if (field.required) {
+      field.required = showIfCheck({ field, variables });
+    }
     const validators = getValidators(field);
     const fieldErrors = validators
       .map(validator => validator(value))
